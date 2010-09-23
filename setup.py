11d8cc557c9ee92d42b82a0d77313118bacdfb3a
Dusty Phillips <dusty@archlinux.ca>
2010-09-23 14:18:30 -0600

    fix dependencies.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         "Pylons>=1.0",
         "Jinja2",
-        "couchdb-python",
+        "CouchDB",
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),

1f43841d09ec296789bd84dfa8e28e4f686d8078
Dusty Phillips <dusty@archlinux.ca>
2011-01-22 16:31:46 -0700

    I never wrote any tests because I didn't really know what I was doing with pylons anyway.

--- prickle/tests/functional/test_projects.py
+++
@@ -1,7 +0,0 @@
-from prickle.tests import *
-
-class TestProjectsController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='projects', action='index'))
-        # Test response...
--- prickle/tests/functional/test_timesheet.py
+++
@@ -1,7 +0,0 @@
-from prickle.tests import *
-
-class TestTimesheetController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='timesheet', action='index'))
-        # Test response...
